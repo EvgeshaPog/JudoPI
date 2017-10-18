94999fe7c7e1561e627bbcbd1062cc4e7a32a1af
tvoix <tvoix@DESKTOP-V4KR3NR>
2017-10-18 15:44:55 +0500

    Класс SQLData

---
+++ Judo/SQLData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace Judo
+{
+    class SQLData
+    {
+             string connectionString = "Data Source=DESKTOP-V4KR3NR;Initial Catalog=новая;Integrated Security=True";// РИНАТ Повелитель Класса SQLDATA
+        //     string connectionString = "Data Source=DESKTOP-V4KR3NR;Initial Catalog=новая;Integrated Security=True";// Евгения Погорелова
+        //     string connectionString = "Data Source=DESKTOP-V4KR3NR;Initial Catalog=новая;Integrated Security=True";// Юлия Носонова
+        //     string connectionString = "Data Source=DESKTOP-V4KR3NR;Initial Catalog=новая;Integrated Security=True";// Кристина Саган
+        //     string connectionString = "Data Source=DESKTOP-V4KR3NR;Initial Catalog=новая;Integrated Security=True";// Екатерина Путенихина
+        //     string connectionString = "Data Source=DESKTOP-V4KR3NR;Initial Catalog=новая;Integrated Security=True";// Алина Сафина
+        //     string connectionString = "Data Source=DESKTOP-V4KR3NR;Initial Catalog=новая;Integrated Security=True";// Евгения Комлева
+        //     string connectionString = "Data Source=DESKTOP-V4KR3NR;Initial Catalog=новая;Integrated Security=True";// Светлана Зарипова
+        //     string connectionString = "Data Source=DESKTOP-V4KR3NR;Initial Catalog=новая;Integrated Security=True";// Анастасия Обливанцева
+
+        public DataTable RunSelect(string zapros)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter dataadapter = new SqlDataAdapter(zapros, connection);
+            DataTable dt = new DataTable();
+            connection.Open();
+            dataadapter.Fill(dt);
+            connection.Close();
+            return dt;
+        }
+
+        public string RunInsertUpdateDelete(string zapros)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection;
+                connection.Open();
+                cmd.CommandText = zapros;
+                cmd.ExecuteNonQuery();
+                cmd.Clone();
+                connection.Close();
+            }
+            catch { return "Произошла ошибка сервера"; }
+            return "Операция прошла успешно";
+         
+
+        }
+    }
+}
