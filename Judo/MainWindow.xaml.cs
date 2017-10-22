83ad11c3c96afe3f4d4255a90c1cfce18d85eead
novik <novik13.03@gmail.com>
2017-10-22 15:19:53 +0500

    Авторизация и регистрация пользователей

--- Judo/MainWindow.xaml.cs
+++ Judo/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,17 +21,41 @@ namespace Judo
     /// </summary>
     public partial class MainWindow : Window
     {
+        SQLData db;
+        DataTable dt;
+
         public MainWindow()
         {
             InitializeComponent();
+            db = new SQLData();
+            dt = new DataTable();
         }
 
         private void butLogin_Click(object sender, RoutedEventArgs e)
         {
-            //Gorod gorod = new Gorod();
-            // gorod.Show();
-            SportC S = new SportC();
-            S.Show();
+            if(tbLogin.Text!="" && tbPassword.Password.ToString()!="")
+            {
+                dt = db.RunSelect("Select FIO, Admin from [User] where Login='" + tbLogin.Text + "' and Password='" + tbPassword.Password.ToString() + "'");
+
+                if (dt.Rows.Count > 0)
+                {
+                    if (dt.Rows[0][1].ToString() == "True")
+                        MessageBox.Show("Админ!");
+                    else
+                        MessageBox.Show("Жюри!");
+                }
+                else
+                    MessageBox.Show("Неверно введен логин или пароль!");
+            }
+            else
+                MessageBox.Show("Заполните все поля!");
+        }
+
+        private void butRegistration_Click(object sender, RoutedEventArgs e)
+        {
+            NewUser nu = new NewUser();
+            nu.Show();
+            Hide();
         }
     }
 }
--- Judo/SQLData.cs
+++ Judo/SQLData.cs
@@ -11,9 +11,9 @@ namespace Judo
 {
     class SQLData
     {
-             string connectionString = "Data Source=DESKTOP-V4KR3NR;Initial Catalog=новая;Integrated Security=True";// РИНАТ Повелитель Класса SQLDATA
+        //     string connectionString = "Data Source=DESKTOP-V4KR3NR;Initial Catalog=новая;Integrated Security=True";// РИНАТ Повелитель Класса SQLDATA
         //     string connectionString = "Data Source=DESKTOP-V4KR3NR;Initial Catalog=новая;Integrated Security=True";// Евгения Погорелова
-        //     string connectionString = "Data Source=DESKTOP-V4KR3NR;Initial Catalog=новая;Integrated Security=True";// Юлия Носонова
+       //      string connectionString = @"Data Source=DESKTOP-K1FLG14\SQLEXPRESS;Initial Catalog=Djudo;Integrated Security=True";// Юлия Носонова
         //     string connectionString = "Data Source=DESKTOP-V4KR3NR;Initial Catalog=новая;Integrated Security=True";// Кристина Саган
         //     string connectionString = "Data Source=DESKTOP-V4KR3NR;Initial Catalog=новая;Integrated Security=True";// Екатерина Путенихина
         //     string connectionString = "Data Source=DESKTOP-V4KR3NR;Initial Catalog=новая;Integrated Security=True";// Алина Сафина
